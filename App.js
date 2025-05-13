ac04c37ce6984d9ad85bbff2c8fe9db0f1bea8d0
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-05-13 19:56:04 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1></h1>
- *          <h2></h2>
- *      </div>
- *      <div id="child">
- *          <h1></h1>
- *          <h2></h2>
- *      </div> 
- * </div> 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, [
-        React.createElement("div", {id:"child"}, [
-        React.createElement("h1", {}, "I'm h1 tag."),
-        React.createElement("h2", {}, "I'm h2 tag.")
+        React.createElement("div", {key : "child1", className:"child"}, [
+        React.createElement("h1", {key : "h1-1"}, "I'm h1 tag."),
+        React.createElement("h2", {key : "h2-1"}, "I'm h2 tag.")
     ]),
     React.createElement(
-        "div", {id:"child"}, [
-        React.createElement("h1", {}, "I'm h1 tag."),
-        React.createElement("h2", {}, "I'm h2 tag.")
+        "div", {key : "child2", className:"child"}, [
+        React.createElement("h1", {key : "h1-2"}, "I'm h1 tag."),
+        React.createElement("h2", {key : "h2-2"}, "I'm h2 tag.")
     ])]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
-console.log(parent);
+// console.log(parent);
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- using File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimazation
+- Minification of Files
+- Bundling of Files
+- Compress Files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different development and poduction build
