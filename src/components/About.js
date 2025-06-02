cdad755f84dc0100452c4dbc204c3424dabc674f
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-06-02 12:39:07 +0530

    episode-07

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <h2>This is Namaste React Web Series.</h2>
+        
+            <User name = {"Amar (function)"} location = {"Busan"} />
+            <UserClass name = {"Amar (class)"} location = {"Beram"} />
+        
+        </div>
+
+    );
+};
+
+export default About;
